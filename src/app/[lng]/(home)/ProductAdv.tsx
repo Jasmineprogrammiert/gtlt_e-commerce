7d25ceb0efde42c2de4c5417ed0c1d01dc5e28f2
Jasmineprogrammiert <514269jasminezeng@gmail.com>
2024-04-28 00:11:00 +0800

    Responsive: TestIncludes

--- src/app/[lng]/(home)/ProductAdv.tsx
+++ src/app/[lng]/(home)/ProductAdv.tsx
@@ -6,7 +6,7 @@ import { data } from './Data/ProductAdvData'
 export default function ProductAdv() {
   return (
     <>
-    <section className="px-10 lg:px-20 pb-20 xl:pb-32 bg-gray-50">
+    <section className="px-10 py-20 lg:px-20 pb-20 xl:pb-32 bg-gray-50">
       <h2 className="home-h2 home-h2-2">Product Advantages</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-5 lg:gap-8">
         {data.map(adv => (
--- src/app/[lng]/(home)/TestIncludes.tsx
+++ src/app/[lng]/(home)/TestIncludes.tsx
@@ -4,17 +4,17 @@ import Link from 'next/link'
 export default function TestIncludes() {
   return (
     <>
-    <section className="p-20 text-white testIncludes">
-      <div className="grid grid-cols-2 gap-8">
+    <section className="px-10 py-20 lg:p-20 text-white testIncludes">
+      <div className="grid lg:grid-cols-2 gap-8">
         <div className="col-span-1">
-          <h2 className="text-5xl mb-10 tracking-wider xl:tracking-widest">Gut Microbiome<br /> Testing Package</h2>
+          <h2 className="text-3xl lg:text-5xl mb-10 tracking-wider xl:tracking-widest">Gut Microbiome<br /> Testing Package</h2>
           <p className="mb-6">
             This is an innovative testing service designed to help you understand the health of your gut. The package includes a simple gut sample collection tool and a detailed testing report which will show your gut microbiome composition and related health information.
           </p>
           <p className="mb-12">
             The testing principle is that we will analyze your gut sample, identify and count different types of microorganisms in the sample. We will use advanced sequencing technology and analysis tools to help you understand your gut microbiome composition, which will provide you with important information about your health and lifestyle.
           </p>
-          <h2 className="text-3xl mb-6 tracking-wider xl:tracking-widest">Benefit Includes:</h2>
+          <h2 className="text-xl lg:text-3xl mb-6 tracking-wider xl:tracking-widest">Benefit Includes:</h2>
           <ol className="leading-relaxed mb-4 list-decimal list-inside">
             <li>
               Personalized nutrition and lifestyle recommendations to help you improve gut health and prevent chronic diseases.
@@ -33,7 +33,7 @@ export default function TestIncludes() {
             <Link href="/tech">More Details</Link>
           </button> 
         </div>
-        <div className="col-span-1">
+        <div className="invisible lg:visible lg:col-span-1">
           <Image
             src="https://images.unsplash.com/photo-1639772823849-6efbd173043c?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bGFifGVufDB8MXwwfHx8Mg%3D%3D"
             alt="1st Image"
