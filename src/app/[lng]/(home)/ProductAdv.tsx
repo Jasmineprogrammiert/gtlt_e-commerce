a765f9079408705813b8e9b7c2cf372d353a9aa1
Jasmineprogrammiert <514269jasminezeng@gmail.com>
2024-04-27 23:58:40 +0800

    Responsive: ProductAdv

--- src/app/[lng]/(home)/ProductAdv.tsx
+++ src/app/[lng]/(home)/ProductAdv.tsx
@@ -6,9 +6,9 @@ import { data } from './Data/ProductAdvData'
 export default function ProductAdv() {
   return (
     <>
-    <section className="px-20 pb-20 xl:pb-32 bg-gray-50">
+    <section className="px-10 lg:px-20 pb-20 xl:pb-32 bg-gray-50">
       <h2 className="home-h2 home-h2-2">Product Advantages</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-5 lg:gap-8">
         {data.map(adv => (
           <div key={adv.id} className="bg-slate-100 my-5 pb-10 flex flex-col items-center text-center">
             <Image
@@ -19,7 +19,7 @@ export default function ProductAdv() {
               height={500}
             />
             <h3 className="text-2xl px-4 mt-9 mb-5 xl:mt-12 xl:mb-8 text-cyan-600">{adv.title}</h3>
-            <p className="text-lg px-3.5 xl:px-10">{adv.description}</p>
+            <p className="text-lg px-4 lg:px-3.5 xl:px-10">{adv.description}</p>
           </div>
         ))}
       </div>
