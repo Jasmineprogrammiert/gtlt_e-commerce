4f9193abe7d141be7033f297fc4cfed318b1b657
Jasmineprogrammiert <514269jasminezeng@gmail.com>
2024-04-28 18:16:47 +0800

    Responsive: /products/[slug]

--- src/app/[lng]/(products)/products/[slug]/components/HealthScores.tsx
+++ src/app/[lng]/(products)/products/[slug]/components/HealthScores.tsx
@@ -34,24 +34,24 @@ export default function HealthScores() {
   }
   return (
     <>
-    <div className="p-20 rounded-lg shadow-lg">
-      <div className="relative flex">
+    <div className="px-5 py-16 md:p-20 rounded-lg shadow-lg">
+      <div className="md:relative md:flex max-md:bg-gray-50">
         <Image
             src="https://images.unsplash.com/photo-1597839219216-a773cb2473e4?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGFyayUyMGJhY2tncm91bmR8ZW58MHwwfDB8fHwy"
             alt="Background Image"
             width={200}
             height={200}
-            className="object-cover object-center w-full h-[30rem] rounded-t-lg"
+            className="max-md:hidden object-cover object-center w-full h-[30rem] rounded-t-lg"
         />
-        <div className="absolute top-4 left-4 p-20 w-3/5">
-            <h2 className="text-4xl xl:text-5xl tracking-wide text-white mb-5 xl:mb-8">
+        <div className="md:absolute md:top-4 md:left-4 px-5 md:p-20 md:w-3/5">
+            <h2 className="text-3xl md:text-4xl xl:text-5xl tracking-wide md:text-white mb-5 xl:mb-8">
               20+ Gut Health Scores
             </h2>
-            <p className="text-lg xl:text-xl text-white">
+            <p className="text-lg xl:text-xl md:text-white">
               Your comprehensive Gut Health analysis evaluates over 20 key factors to give you a comprehensive overview of your gut health. It covers various functions, including digestion, gut lining strength, and microbial diversity, and forms the basis for your personalized recommendations to help you support your gut{`'`}s needs.
             </p>
         </div>
-        <div className="absolute bottom-2 right-4 p-20 w-2/5 xl:w-[33%]">
+        <div className="max-md:hidden absolute bottom-2 right-4 p-20 w-2/5 xl:w-[33%]">
           <Image
               src="https://images.unsplash.com/photo-1614587113639-67c78332bb39?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGRhcmslMjBwcm9kdWN0fGVufDB8MXwwfHx8Mg%3D%3D"
               alt="Bottom Right Image"
@@ -62,7 +62,7 @@ export default function HealthScores() {
         </div>
       </div>
       
-      <div className="grid grid-cols-2 gap-x-10 gap-y-4 p-16 bg-gray-900 text-slate-50">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-5 md:gap-y-4 px-5 py-8 md:p-16 bg-gray-50 md:bg-gray-900 md:text-slate-50">
         <div className="hs-collapsible">
           <div>
             <button onClick={toggleCollapse1}>
