c15db1c640a25f307586cdb656e706b3af2df058
Jasmineprogrammiert <514269jasminezeng@gmail.com>
2024-04-28 19:31:31 +0800

    Responsive: /questions/[index]

--- src/app/[lng]/(explore)/questions/[index]/page.tsx
+++ src/app/[lng]/(explore)/questions/[index]/page.tsx
@@ -10,19 +10,19 @@ export default function FrequentlyAskedTemplate({
     {[CardData[Number(params.index)]].map((card, i) => (
       <section key={i} className="px-5 py-16 md:p-20 flex flex-wrap bg-gray-50">
         <div className="w-full p-4 text-center">
-          <h3 className="text-5xl tracking-wide mb-16 text-cyan-600">
+          <h3 className="text-4xl md:text-5xl tracking-wide mb-12 md:mb-16 text-cyan-600">
             {card.title}
           </h3>
-          <h4 className="text-3xl tracking-wide mb-7 text-start">
+          <h4 className="text-2xl md:text-3xl tracking-wide md:mb-7 text-start">
             Articles
           </h4>
         </div>
         {card.subtitles.map((s, subIndex) => (
           <div 
-            className="w-1/2 p-4 max-h-60 overflow-y-auto mb-16"
+            className="md:w-1/2 p-4 max-h-60 overflow-y-auto mb-8 md:mb-16"
             key={subIndex}
           >
-            <h4 className="mb-7 text-lg xl:text-xl font-medium">
+            <h4 className="mb-4 md:mb-7 text-2xl md:text-lg xl:text-xl md:font-medium">
               {s.subtitle}
             </h4>
             <p className="text-base leading-snug mb-5">
